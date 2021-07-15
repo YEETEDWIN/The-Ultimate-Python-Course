c861b957b491ae1af974266f9ef19d5523446156
Y^^TED <76407715+YEETEDWIN@users.noreply.github.com>
2021-07-15 22:21:44 +0200

    Create main.py

---
+++ Beginners/Course 1/main.py
@@ -0,0 +1,33 @@
+"""
+Beginners, Python Course 1
+(c) 2021 YEETEDWIN
+website coming later
+"""
+
+# Course 1: Printing and Variables
+# Course 1a - Printing:
+
+# <-- Allows you to comment/type non-python code... It can be used for organizing your code
+
+"""
+^ Allows you to comment/type non-python code multiple lines
+
+One basic thing in python is that the normal python without imports such as pygame or pgzero you can only print text and make text games (if you want to create 
+games)
+The console is where your python code is printed.
+"""
+print("This is how you print on the console. Inside these quotation you can put the text you want to print")
+print("It can be either printed in these quotations marks or these --> '' ")
+
+# Course 1b - Variables:
+
+# variables are things that can have data inside.
+# variable allow you to place things inside like integers, text or other data... An example can be:
+
+var = 'lol' # the quotation marks make it a string/text that can be printed... It doesn't have to be called var, it can be anything.
+print(var) # this will print the data inside which is lol. If lol wasn't in quotation marks then it will make a syntal error. If var is with the marks then it will  
+# print var on the console
+# Now you can try! this are the 2 most essential things in Python. one more thing, when you make a variable thats a number like 5 then you don't need quotation 
+# marks, as when you are adding (which you will learn in the other courses) 2 numbers but to print this variable you need to do:
+num1 = 5
+print(int(num1)) # int() means integer if the other way around you use str()
